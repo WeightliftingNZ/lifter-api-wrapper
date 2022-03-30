d8ba035cad44b67c61ca6f4d18a1990cf551bc96
Shivan Sivakumarnan <51132467+ShivanS93@users.noreply.github.com>
2022-03-30 13:25:28 +1300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name="lifter-api-wrapper",
     version="0.1.0",
-    description="A python wrapper to access Lifer API",
+    description="A python wrapper to access Lifter API",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ChristchurchCityWeightlifting/lifter-api-wrapper",
